19b78b845b37ad605bb3d8e00841ff215879728b
Al-Amin Firdows <alaminfirdows@gmail.com>
2024-09-02 16:55:01 +0600

    fix: replace state with ref

--- resources/js/Components/BackToTop.tsx
+++ resources/js/Components/BackToTop.tsx
@@ -1,19 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import { ChevronUpIcon } from '@heroicons/react/24/outline';
 
 const BackToTop = () => {
-  const [showTopButton, setShowTopButton] = useState(false);
+  const buttonRef = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 900) {
-        setShowTopButton(true);
-      } else {
-        setShowTopButton(false);
+      if (buttonRef.current) {
+        buttonRef.current.style.display =
+          window.scrollY > 900 ? 'block' : 'none';
       }
     };
 
     window.addEventListener('scroll', handleScroll);
+
+    handleScroll(); // Initial check on component mount
 
     return () => {
       window.removeEventListener('scroll', handleScroll);
@@ -25,14 +26,14 @@ const BackToTop = () => {
   };
 
   return (
-    showTopButton && (
-      <button
-        onClick={scrollToTop}
-        className="fixed bottom-8 right-8 bg-indigo-600 text-white p-2 rounded-full shadow-lg hover:bg-indigo-500 focus:outline-none dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-gray-700"
-      >
-        <ChevronUpIcon className="h-6 w-6" />
-      </button>
-    )
+    <button
+      ref={buttonRef}
+      onClick={scrollToTop}
+      style={{ display: 'none' }}
+      className="fixed bottom-8 right-8 bg-indigo-600 text-white p-2 rounded-full shadow-lg hover:bg-indigo-500 focus:outline-none dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-gray-700"
+    >
+      <ChevronUpIcon className="h-6 w-6" />
+    </button>
   );
 };
 
